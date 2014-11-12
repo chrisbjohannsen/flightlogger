082fbbc00412867001db48360f880a17a1d769af
Christopher Johannsen <chrisbjohannsen@gmail.com>
2014-11-11 17:22:03 -0800

    adding more health testing

--- FL2.cpp
+++ FL2.cpp
@@ -56,5 +56,13 @@ void healthCheck() {
 	delay(500);               // wait for a second
 	digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
 	delay(500);
+	digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+	delay(1500);               // wait for a second
+	digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+	delay(1500);
+	digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+	delay(2500);               // wait for a second
+	digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+	delay(2500);
 }
 
